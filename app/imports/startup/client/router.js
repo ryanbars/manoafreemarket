fb2387b7cfac67cdc38781b66ba3307b8a916cb1
Ryan Barsatan <ryanbinfo@gmail.com>
2016-10-25 10:51:55 -1000

    first commit

---
+++ app/imports/startup/client/router.js
@@ -0,0 +1,43 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+  name: 'Home_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Home_Page' });
+  },
+});
+
+FlowRouter.route('/list', {
+  name: 'List_Stuff_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+  },
+});
+
+FlowRouter.route('/add', {
+  name: 'Add_Stuff_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
+  },
+});
+
+FlowRouter.route('/item/:_id', {
+  name: 'Item_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Item_Page' });
+  },
+});
+
+FlowRouter.route('/post', {
+  name: 'Post_Item',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Post_Item' });
+  },
+});
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
+  },
+};
